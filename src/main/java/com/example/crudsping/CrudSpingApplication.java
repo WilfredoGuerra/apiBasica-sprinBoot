a585f86a06e572470a249fb61a3359c28a556324
Wilfredo Guerra <willigue@gmail.com>
2023-06-10 14:47:24 -0400

    proyecto api en srpin boot

---
+++ src/main/java/com/example/crudsping/CrudSpingApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudsping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpingApplication.class, args);
+	}
+
+}
